3324d10efca0337d4feda72c4d683460c3b3c9d0
joanvic <117447073+joanvic@users.noreply.github.com>
2023-05-17 14:37:28 +0800

    Add files via upload

---
+++ AttendanceGUI/AttendanceGUI/Tab.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AttendanceGUI
+{
+    public partial class Tab : Form
+    {
+        public List<string> attendanceList;
+        public Tab()
+        {
+            InitializeComponent();
+
+            attendanceList = new List<string>() { "John Doe", "nelson" };
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string studentName;
+
+            if (!attendanceList.Contains(studentName))
+            {
+                // pang present
+                attendanceList.Add(studentName);
+                MessageBox.Show($"{studentName} is marked as present.");
+            }
+            else
+            {
+
+                MessageBox.Show($"{studentName} is already marked as present.");
+            }
+        }
+
+
+
+        public void Attendance_ParentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AttendanceTab_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
